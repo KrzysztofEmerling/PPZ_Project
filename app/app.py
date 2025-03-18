cd9be762eababb95efd1992f91bed8dc96e59d8e
agnieszkabies <agnieszkabies@gmail.com>
2025-03-18 09:28:11 +0100

    Implementacja modelu bazy danych

--- app/app.py
+++ app/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from models import db, init_db
+import os
 
 app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
+DB_PATH = os.path.join(BASE_DIR, "../database.db") 
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_PATH}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+init_db(app)
 
 @app.route('/')
 def home():
