f1eef885f42db40c42f9abf9bf3ab9b22b94fa0c
Mr Chamster <s.duda03@gmail.com>
2025-04-01 08:27:25 +0200

    Obsługa logiki rejestracji i logowania
    login i register prawidłowo przekazują dane, a app obsługuje logikę

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import flask as f
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import inspect, text
-import os
+# import os
 from models import User, Admin, GameResult, db
 
 app = f.Flask(__name__)
@@ -24,16 +24,61 @@ def register():
 
 @app.route('/handle_login', methods=['POST'])
 def handle_login():
-    user_name = f.request.form.get('floatingInput')  
-    password = f.request.form.get('floatingPassword')  
+    email_input_login = f.request.form.get('email_input_login')
+    password_input_login = f.request.form.get('password_input_login')
 
-    user = db.session.query(User).filter_by(email=user_name).first()
+    print(email_input_login, password_input_login)
 
-
-    if user:
-        return f"Przesłane dane:<br>Użytkownik: {user_name}<br>Hasło: {password}"
+    user_exists = User.query.filter_by(email=email_input_login, password=password_input_login).first()
+    if user_exists:
+        #has to be handled better
+        return "User exists in database"
     else:
-        return "Nie ma takiego użytkownika"
+        #has to be handled better
+        return "User doesn't exist in database"
+
+
+    # user = db.session.query(User).filter_by(email=email_input_login).first()
+    # print(type(user))
+
+
+    # if user:
+    #     return f"Przesłane dane:<br>Użytkownik: {email_input_login}<br>Hasło: {password_input_login}"
+    # else:
+    #     return "Nie ma takiego użytkownika"
+    
+@app.route('/handle_register', methods=['POST'])
+def handle_register():
+
+    email_input_reg = f.request.form.get('email_input_reg')
+    username_input_reg = f.request.form.get('username_input_reg')
+    password_input_reg = f.request.form.get('password_input_reg')
+    confirm_password_input_reg = f.request.form.get('confirm_password_input_reg')
+    print(email_input_reg, username_input_reg, password_input_reg, confirm_password_input_reg)
+
+
+    if password_input_reg != confirm_password_input_reg:
+        #has to be handled better
+        return "Passwords don't match"
+    
+    email_exists = db.session.query(User).filter_by(email=email_input_reg).first()
+    if email_exists:
+        #has to be handled better
+        return "Email already exists"
+    
+    username_exists = db.session.query(User).filter_by(username=username_input_reg).first()
+    if username_exists:
+        #has to be handled better
+        return "Username already exists"
+    
+    #if user can be added
+    user = User(email=email_input_reg, username=username_input_reg, password=password_input_reg)
+    db.session.add(user)
+    db.session.commit()
+    
+    return f.render_template('login.html')
+
+    # return "Stuff" #stuff shown on site handle_register. Should it put user to login page?
 
 @app.route('/debug')
 def show_tables():
